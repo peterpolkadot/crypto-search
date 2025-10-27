0ffbd6ad122cf03ab6d7a8ede8c28373cd296194
peterpolkadot <154844362+peterpolkadot@users.noreply.github.com>
2025-10-28 07:59:47 +1000

    Update [symbol].js

--- pages/coins/[symbol].js
+++ pages/coins/[symbol].js
@@ -8,7 +8,7 @@ export default function CoinPage() {
   const [loading, setLoading] = useState(true);
 
   const API_BASE =
-    "https://script.google.com/macros/s/AKfycbxAa7Rp2iZUjR7TNjiHH4zLggFotCGuKXb_KtgLECH4eCMKujeYMI0BD5hQKvUiirw/exec";
+    "https://script.google.com/macros/s/AKfycbzY1t4l6Osq7CQf8v5kwtvPnuWfBl1-GzD-KXz3LUl1zD_qyQVUa9T9o1LCzoOO3caP/exec";
 
   useEffect(() => {
     if (!symbol) return; // Wait until route param is ready
