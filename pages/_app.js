82c09dfd5a30ed1082cfa24fd297356a7139627f
peterpolkadot <154844362+peterpolkadot@users.noreply.github.com>
2025-10-28 10:16:59 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  )
 }
